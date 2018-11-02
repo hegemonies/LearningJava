4f682c90104bb68dc94162ad4422873194a582ab
hegemonies <hegemonies@yandex.ru>
2018-11-02 23:35:19 +0700

    Fix CSVReader

--- csvBomb/src/main/java/CSVReader/CSVReader.java
+++ csvBomb/src/main/java/CSVReader/CSVReader.java
@@ -8,18 +8,21 @@ public class CSVReader {
         try {
             FileReader reader = new FileReader(fileName);
 
-//            char[] buffer = new char[256];
-            String buffer = new String();
+            char[] buffer = new char[256];
+//            String buffer = new String();
             char buf;
             try {
-                while ((buf = (char) reader.read()) > 0) {
-//                    buffer += buf;
-                    String tmp = new StringBuilder().append(buf).toString();
-                    buffer += tmp;
-                }
+//                while ((buf = (char) reader.read()) > 0) {
+////                    buffer += buf;
+//                    String tmp = new StringBuilder().append(buf).toString();
+//                    buffer += tmp;
+//                }
 //                reader.read(buffer);
+                while (reader.read(buffer) > 0) {
 
-                return buffer;
+                }
+
+                return new String(buffer);
             } catch (Exception exc) {
 
             }
