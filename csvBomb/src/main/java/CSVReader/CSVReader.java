857aba2df9f2244195abc781201498f6c321e704
hegemonies <hegemonies@yandex.ru>
2018-11-02 23:01:19 +0700

    Added to CSVReader

--- csvBomb/src/main/java/CSVReader/CSVReader.java
+++ csvBomb/src/main/java/CSVReader/CSVReader.java
@@ -4,13 +4,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVReader {
-    public static void toRead(String fileName) throws Exception {
+    public static String toRead(String fileName) throws Exception {
         try {
             FileReader reader = new FileReader(fileName);
 
-            char[] buffer = new char[256];
-            while (reader.read(buffer) > 0) {
-                
+//            char[] buffer = new char[256];
+            String buffer = new String();
+            char buf;
+            try {
+                while ((buf = (char) reader.read()) > 0) {
+//                    buffer += buf;
+                    String tmp = new StringBuilder().append(buf).toString();
+                    buffer += tmp;
+                }
+//                reader.read(buffer);
+
+                return buffer;
+            } catch (Exception exc) {
+
             }
 
             reader.close();
@@ -18,5 +29,7 @@ public class CSVReader {
             throw new Exception(exc.getMessage());
         }
 
+        return null;
     }
+
 }
