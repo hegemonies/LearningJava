19eab78be14b75ee43e3f2557d1193e090b16a24
hegemonies <hegemonies@yandex.ru>
2019-03-10 19:32:50 +0700

    Add to .gitignore and add application.properties for CPB

--- CPB/src/main/java/CloudPhoneBook/Repos/RepoIndividuals.java
+++ CPB/src/main/java/CloudPhoneBook/Repos/RepoIndividuals.java
@@ -3,6 +3,7 @@ package CloudPhoneBook.Repos;
 import CloudPhoneBook.Individual;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface RepoIndividuals extends MongoRepository<Individual, String> {
     public Individual findByName(String name);
 }
